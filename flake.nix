44290b7efcf2db65e405083270794fc626da6d8d
Franz Pletz <fpletz@fnordicwalking.de>
2024-02-27 22:59:44 +0100

    refactor flake

--- flake.nix
+++ flake.nix
@@ -1,34 +1,25 @@
 {
   description = "Bad Gateway";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.nginxQuic =
-      nixpkgs.legacyPackages.x86_64-linux.nginxQuic.overrideAttrs (attrs: {
-        patches = attrs.patches ++ [ ./patches/bad_gateway_nginx_1.25.4.patch ];
-      });
-    packages.x86_64-linux.nginxStable =
-      nixpkgs.legacyPackages.x86_64-linux.nginxStable.overrideAttrs (attrs: {
-        patches = attrs.patches ++ [ ./patches/bad_gateway_nginx_1.25.4.patch ];
-      });
-    packages.x86_64-linux.nginxMainline =
-      nixpkgs.legacyPackages.x86_64-linux.nginxMainline.overrideAttrs (attrs: {
-        patches = attrs.patches ++ [ ./patches/bad_gateway_nginx_1.25.4.patch ];
-      });
-    packages.x86_64-linux.default = self.packages.x86_64-linux.nginxStable;
-
-    overlays.default = final: prev: {
-      nginxQuic = prev.nginxQuic.overrideAttrs (attrs: {
-        patches = attrs.patches ++ [ ./patches/bad_gateway_nginx_1.25.4.patch ];
-      });
-      nginxStable = prev.nginxStable.overrideAttrs (attrs: {
-        patches = attrs.patches ++ [ ./patches/bad_gateway_nginx_1.25.4.patch ];
-      });
-      nginxMainline = prev.nginxMainline.overrideAttrs (attrs: {
-        patches = attrs.patches ++ [ ./patches/bad_gateway_nginx_1.25.4.patch ];
-      });
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
+
+  outputs = { self, nixpkgs, systems }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+      eachNginx = nixpkgs.lib.genAttrs [ "nginxQuic" "nginxStable" "nginxMainline" ];
+      pkgs = import nixpkgs { overlays = [ self.overlays.default ]; };
+    in
+    {
+      packages = eachSystem (_system: eachNginx (nginxAttr: pkgs.${nginxAttr}));
+
+      overlays.default = _final: prev:
+        eachNginx (nginxAttr:
+          prev.${nginxAttr}.overrideAttrs (attrs: {
+            patches = attrs.patches ++ [ ./patches/bad_gateway_nginx_1.25.4.patch ];
+          })
+        );
+    };
 }
